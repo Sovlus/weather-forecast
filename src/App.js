affe35e6eacb2f615c6df4c331b9731b82d0468b
Sovlus <kubbaz122@gmail.com>
2023-04-17 22:51:49 +0200

    added weather api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import axios from 'axios';
 import './App.css';
+
+function getWeatherData() {
+  const apiKey = '076e4a179712e35dc665c307d7b2b364';
+  const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=${076e4a179712e35dc665c307d7b2b364}';
+
+  return axios.get(apiUrl);
+}
 
 function App() {
   return (
