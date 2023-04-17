46b1ea83158e211d4e535117ac239d976cc75e70
Sovlus <kubbaz122@gmail.com>
2023-04-17 23:03:19 +0200

    added search button and form

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
@@ -9,13 +10,43 @@ function getWeatherData() {
 }
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
 
-      </header>
+  useEffect(() => {
+    if (city) {
+      setLoading(true);
+      axios
+        .get('${BASE_URL}weather?q=${city}&units=metric&appid=${076e4a179712e35dc665c307d7b2b364}')
+        .then((response) => {
+          setWeather(response.data);
+          setLoading(false);
+        })
+        .catch((error) => {
+          setError(error.message);
+          setLoading(false);
+        });
+    }
+  }, [city]);
+
+  const handleSearch = (event) => {
+    event.preventDefault();
+    setCity(event.target.elements.city.value);
+  };
+
+  return (
+    <div className='App'>
+      <h1>Pogoda</h1>
+      <form onSubmit={handleSearch}>
+        <input type='text' name='city' placeholder='Wpisz nazwę miasta' />
+        <button type='submit'>Wyszukaj</button>
+      </form>
+
+
     </div>
-  );
+  )
 }
 
 export default App;
